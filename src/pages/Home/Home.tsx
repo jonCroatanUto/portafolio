b2496dc215fb0dd1262a1d5ec2dbebb4fcfdd4ab
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2021-12-30 19:06:46 +0100

    invert color  div follow mouse

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,21 @@
+import React, { useState } from "react";
+import "./homeStyle.css";
 function Home() {
-  return <div>this is Home</div>;
+  const [pos, setPos] = useState({ posX: "500px", posY: "300px" });
+  function move(e: any) {
+    let posX = e.nativeEvent.offsetX - 300;
+    let posY = e.nativeEvent.offsetY - 300;
+    setPos({
+      posX: `${posX.toString()}px`,
+      posY: `${posY.toString()}px`,
+    });
+  }
+  return (
+    <>
+      <h1>this is Home</h1>
+      <div onMouseMove={move} className="getmousePos"></div>
+      <div style={{ left: pos.posX, top: pos.posY }} className="boxColor"></div>
+    </>
+  );
 }
 export default Home;
