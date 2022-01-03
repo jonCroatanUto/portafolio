cccb513c4cf7e1f6ccfa806569458137b502d41b
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-03 14:59:29 +0100

    titles distribution

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,24 @@
+import React from "react";
+import "./home.css";
 function Home() {
-  return <div>this is Home</div>;
+  return (
+    <div className="menu">
+      <div className="FullStackDeveloper">
+        <h1>FULL STACK </h1>
+        <h1>DEVELOPER </h1>
+      </div>
+      <div className="motionGraphics">
+        <h1>MOTION</h1>
+        <h1>GRAPHICS </h1>
+        <h1>DESIGNER </h1>
+      </div>
+      <div className="foundMe">
+        <h1>CONTACT </h1>
+      </div>
+      <div className="me">
+        <h1>ABOUT ME </h1>
+      </div>
+    </div>
+  );
 }
 export default Home;
