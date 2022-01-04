c91317abdf2e8ade93d67cb7cd7888f77ea3faca
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-04 18:14:29 +0100

    details

--- src/components/HomeMenu/HomeMenu.tsx
+++ src/components/HomeMenu/HomeMenu.tsx
@@ -12,7 +12,7 @@ function HomeMenu() {
     <div className="menu">
       <div
         onMouseOver={() => hover("--full-stack-font-H1-axis-weight", "700")}
-        onMouseOut={() => hover("--full-stack-font-H1-axis-weight", "200")}
+        onMouseOut={() => hover("--full-stack-font-H1-axis-weight", "100")}
         className="FullStackDeveloper"
       >
         <h1>FULLSTACK </h1>
@@ -28,12 +28,12 @@ function HomeMenu() {
         className="motionGraphics"
       >
         <h1>MOTION</h1>
-        <h1>GRAPHICS </h1>
+        <h1>GRAPHIC</h1>
         <h1>DESIGNER </h1>
       </div>
       <div
         onMouseOver={() => hover("--me-stack-font-H1-axis-weight", "700")}
-        onMouseOut={() => hover("--me-stack-font-H1-axis-weight", "200")}
+        onMouseOut={() => hover("--me-stack-font-H1-axis-weight", "100")}
         className="me"
       >
         <h1>ABOUT ME </h1>
