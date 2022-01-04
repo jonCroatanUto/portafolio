1f9dea4bce159263e5449e511f8723d83d65cb28
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-04 12:42:19 +0100

    preparing hover of Home types

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./home.css";
 function Home() {
+  function hover() {
+    // document.body.style.--full-stack-font-H1-axis-weight
+    console.log("hola");
+  }
   return (
     <div className="menu">
-      <div className="FullStackDeveloper">
+      <div onMouseOver={hover} className="FullStackDeveloper">
         <h1 className="fullStack">FULLSTACK </h1>
         <h1 className="fullStack">DEVELOPER </h1>
       </div>
