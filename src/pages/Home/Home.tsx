5272a0e4850feb35ad7c7c937b7e8affb42851e4
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-04 13:22:22 +0100

    conect hover with variable weight

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,47 @@
 import React from "react";
 import "./home.css";
 function Home() {
-  function hover() {
-    // document.body.style.--full-stack-font-H1-axis-weight
-    console.log("hola");
+  let fullStackClass = "fullStack";
+  function hover(globalVariable: string, value: string) {
+    let fontWeigth = document.body.style;
+    fontWeigth.setProperty(globalVariable, value);
+    // fullStackClass = "FullStackDeveloperAnimation";
   }
   return (
     <div className="menu">
-      <div onMouseOver={hover} className="FullStackDeveloper">
-        <h1 className="fullStack">FULLSTACK </h1>
-        <h1 className="fullStack">DEVELOPER </h1>
+      <div
+        onMouseOver={() => hover("--full-stack-font-H1-axis-weight", "700")}
+        onMouseOut={() => hover("--full-stack-font-H1-axis-weight", "200")}
+        className="FullStackDeveloper"
+      >
+        <h1>FULLSTACK </h1>
+        <h1>DEVELOPER </h1>
       </div>
-      <div className="motionGraphics">
+      <div
+        onMouseOver={() =>
+          hover("--animation-design-font-H1-axis-weight", "900")
+        }
+        onMouseOut={() =>
+          hover("--animation-design-font-H1-axis-weight", "100")
+        }
+        className="motionGraphics"
+      >
         <h1>MOTION</h1>
         <h1>GRAPHICS </h1>
         <h1>DESIGNER </h1>
       </div>
-      <div className="me">
+      <div
+        onMouseOver={() => hover("--me-stack-font-H1-axis-weight", "700")}
+        onMouseOut={() => hover("--me-stack-font-H1-axis-weight", "200")}
+        className="me"
+      >
         <h1>ABOUT ME </h1>
       </div>
-      <div className="contact">
+      <div
+        onMouseOver={() => hover("--contact-stack-font-H1-axis-weight", "900")}
+        onMouseOut={() => hover("--contact-stack-font-H1-axis-weight", "100")}
+        className="contact"
+      >
         <h1>CONTACT </h1>
       </div>
     </div>
