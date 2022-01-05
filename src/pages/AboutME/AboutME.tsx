4088947a5db2e85903fa19d405b65879e932343f
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-01-05 18:18:36 +0100

    display keyword about my description following layout design-->done; adding colors,ajust details-->proces

--- src/pages/AboutME/AboutME.tsx
+++ src/pages/AboutME/AboutME.tsx
@@ -1,4 +1,49 @@
+import React from "react";
+import "./aboutme.css";
+
 function AboutME() {
-  return <div style={{ color: "white" }}>hi I'm aboutme</div>;
+  return (
+    <div className="keyWordsBox">
+      <div className="keyWords1">
+        <h1>Growth</h1>
+      </div>
+      <div className="keyWords2">
+        <h1>Resarcher</h1>
+        <h1>Team-building</h1>
+      </div>
+      <div className="keyWords3">
+        <h1>Comunication</h1>
+      </div>
+      <div className="keyWords4">
+        <h1>Creativity</h1>
+        <h1>Self-learning</h1>
+        <h1>Active-listening</h1>
+      </div>
+      <div className="keyWords5">
+        <h1>SCRUM</h1>
+        <h1>Github</h1>
+        <h1>Github projects</h1>
+      </div>
+      <div className="keyWords6">
+        <h1>NPM</h1>
+        <h1>Node.js</h1>
+        <h1>React.js</h1>
+        <h1>Bootstrap</h1>
+      </div>
+      <div className="keyWords7">
+        <h1>REDUX</h1>
+        <h1>MongoDB</h1>
+        <h1>PHP</h1>
+        <h1>mySQL</h1>
+      </div>
+      <div className="keyWords8">
+        <h1>Express.js</h1>
+        <h1>NETLIFY</h1>
+      </div>
+      <div className="keyWords9">
+        <h1>HEROKU</h1>
+      </div>
+    </div>
+  );
 }
 export default AboutME;
