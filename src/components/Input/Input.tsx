1edae5efa3eb71a06b07997af8cd1fd4edb1d099
JonCroatan <43881219+jonCroatanUto@users.noreply.github.com>
2022-02-04 12:02:25 +0100

    adding reusable custom input

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "./styles.css";
+
+function Input({
+  type,
+  id,
+  placeholder,
+  value,
+  handleChange,
+  label,
+}: {
+  type: string;
+  id: string;
+  value: string;
+  placeholder: string;
+  handleChange: (e: any) => void;
+  label: string;
+}) {
+  return (
+    <>
+      <div className="field">
+        <input
+          className="input-field"
+          type={type}
+          id={id}
+          value={value}
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+        <label className="input-label" htmlFor={id}>
+          {label}
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default Input;
